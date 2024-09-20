924604d8fb57a4d7a843b174c5bc9112d138834a
Paulo Guilherme <paulogoncalves.dev@gmail.com>
2024-09-19 22:10:39 -0300

    feat: Adiciona lógica para login

--- src/Web/ExpensesManager.Web/Controllers/LoginController.cs
+++ src/Web/ExpensesManager.Web/Controllers/LoginController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExpensesManager.Application.BusinessRules.Interfaces.User;
+using ExpensesManager.Application.Services.Cryptography;
+using ExpensesManager.Domain.Entities;
+using ExpensesManager.Exceptions.Exceptions;
+using ExpensesManager.Exceptions.ResourcesMessage;
+using ExpensesManager.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ExpensesManager.Web.Controllers
 {
@@ -7,6 +13,37 @@ namespace ExpensesManager.Web.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<ActionResult> Login([FromForm] UserModel userModel, [FromServices] IGetUser getUser, [FromServices] PasswordEncryption passwordEncryption)
+        {
+            try
+            {
+                User user = await getUser.Execute(userModel.Mail);
+
+                ValidateUser(user, userModel.Password, passwordEncryption);
+
+                return RedirectToAction("Index", "Dashboard");
+            }
+            catch (Exception ex)
+            {
+                if (ex is ValidationException)
+                {
+                    ValidationException validationsError = ex as ValidationException;
+                    TempData["ErrorMessage"] = validationsError.ErrorMessage;
+
+                    return RedirectToAction("Index");
+                }
+
+                TempData["ErrorMessage"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+        private static void ValidateUser(User user, string passwordModel, [FromServices] PasswordEncryption passwordEncryption)
+        {
+            if (user == null || !user.Password.Equals(passwordEncryption.Encrypt(passwordModel, user.Salt)))
+                throw new ValidationException(VALIDATIONMSG.EXISTING_USER);
         }
     }
 }
