8568151ecd2cf3772a5ba6afd4b294ab77e806f0
Paulo Guilherme <paulogoncalves.dev@gmail.com>
2025-01-16 22:18:10 -0300

    feat: adiciona fila para consumo de payload

--- src/Functions/ExpensesManager.SendMail/SendMail.cs
+++ src/Functions/ExpensesManager.SendMail/SendMail.cs
@@ -1,6 +1,8 @@
+using ExpensesManager.Events.Services.MailSender;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Text.Json;
 
 namespace ExpensesManager.SendMail
 {
@@ -8,16 +10,23 @@ namespace ExpensesManager.SendMail
     {
         private readonly ILogger _logger;
 
-        public SendMail(ILoggerFactory loggerFactory)
+        private readonly IMailSender _mailSender;
+
+        public SendMail(ILoggerFactory loggerFactory, IMailSender mailSender)
         {
             _logger = loggerFactory.CreateLogger<SendMail>();
+            _mailSender = mailSender;
         }
 
         [Function("SendMail")]
-        public void Run([RabbitMQTrigger("QueueName", ConnectionStringSetting = "RabbitMQConnectionURL")] string payload)
+        public void Run([RabbitMQTrigger("expensesmanager.sendmailservice.queue", ConnectionStringSetting = "RabbitMQConnectionURL")] string payload)
         {
             try
             {
+                using JsonDocument payloadDocument = JsonDocument.Parse(payload);
+
+                _mailSender.SendMail(payloadDocument);
+
                 _logger.LogInformation($"Queue trigger function processed: {payload}");
             }
             catch (Exception ex)
