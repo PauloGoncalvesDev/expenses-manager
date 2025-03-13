a67952ce6366f676546ae8a99ab485f31a465120
Paulo Guilherme <paulogoncalves.dev@gmail.com>
2025-03-13 20:47:05 -0300

    feat: adiciona testes unitários para transações

---
+++ tests/ExpensesManager.TestUtility/Entities/TransactionBuilder.cs
@@ -0,0 +1,22 @@
+﻿using Bogus;
+using ExpensesManager.Domain.Entities;
+using ExpensesManager.Domain.Enum;
+
+namespace ExpensesManager.TestUtility.Entities
+{
+    public class TransactionBuilder
+    {
+        public static Transaction GenerateTransaction(TransactionType transactionType = TransactionType.Investment)
+        {
+            Transaction transaction = new Faker<Transaction>()
+                .RuleFor(r => r.Id, _ => 1)
+                .RuleFor(r => r.CategoryId, _ => 1)
+                .RuleFor(r => r.TransactionDate, b => b.Date.Recent(1))
+                .RuleFor(r => r.Amount, b => b.Finance.Amount())
+                .RuleFor(r => r.Description, b => b.Commerce.ProductDescription())
+                .RuleFor(r => r.Type, _ => transactionType);
+
+            return transaction;
+        }
+    }
+}
