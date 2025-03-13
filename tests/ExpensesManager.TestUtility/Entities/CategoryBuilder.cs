ba450a39482f5700282b010668bfe4a62394bee2
Paulo Guilherme <paulogoncalves.dev@gmail.com>
2025-03-13 20:46:47 -0300

    feat: Adiciona testes unitários para categorias

---
+++ tests/ExpensesManager.TestUtility/Entities/CategoryBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Bogus;
+using ExpensesManager.Domain.Entities;
+using ExpensesManager.Domain.Enum;
+
+namespace ExpensesManager.TestUtility.Entities
+{
+    public class CategoryBuilder
+    {
+        public static Category GenerateCategory(CategoryType categoryType = CategoryType.Expenses)
+        {
+            Category category = new Faker<Category>()
+                .RuleFor(r => r.Title, f => f.Commerce.ProductName())
+                .RuleFor(r => r.Type, _ => categoryType);
+
+            return category;
+        }
+    }
+}
---
+++ tests/ExpensesManager.Validators.Test/CategoryTests/ValidateCategoryTest.cs
@@ -0,0 +1,60 @@
+﻿using ExpensesManager.Application.Validators;
+using ExpensesManager.Domain.Entities;
+using ExpensesManager.Domain.Enum;
+using ExpensesManager.Exceptions.Exceptions;
+using ExpensesManager.Exceptions.ResourcesMessage;
+using ExpensesManager.TestUtility.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace ExpensesManager.Validators.Test.CategoryTests
+{
+    public class ValidateCategoryTest
+    {
+        [Fact]
+        public void Validate_CreateCategory_Success()
+        {
+            Category category = CategoryBuilder.GenerateCategory();
+
+            Exception exception = Record.Exception(() => CategoryValidator.Validate(category));
+
+            exception.Should().BeNull();
+        }
+
+        [Theory]
+        [InlineData(CategoryType.Income)]
+        [InlineData(CategoryType.Expenses)]
+        public void Validate_CreateCategory_Types_Success(CategoryType categoryType)
+        {
+            Category category = CategoryBuilder.GenerateCategory(categoryType);
+
+            Exception exception = Record.Exception(() => CategoryValidator.Validate(category));
+
+            exception.Should().BeNull();
+        }
+
+        [Fact]
+        public void Validate_CreateCategory_Should_Throw_Exception_When_Title_Is_Empty()
+        {
+            Category category = CategoryBuilder.GenerateCategory();
+
+            category.Title = string.Empty;
+
+            Action action = () => CategoryValidator.Validate(category);
+
+            action.Should().Throw<ValidationException>().WithMessage(VALIDATIONMSG.TITLE_EMPTY);
+        }
+
+        [Fact]
+        public void Validate_CreateCategory_Should_Throw_Exception_When_Title_Only_Number()
+        {
+            Category category = CategoryBuilder.GenerateCategory();
+
+            category.Title = "123";
+
+            Action action = () => CategoryValidator.Validate(category);
+
+            action.Should().Throw<ValidationException>().WithMessage(VALIDATIONMSG.TITLE_NUMBER);
+        }
+    }
+}
